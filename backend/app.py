aec05e0cea4c4d4a99e0f7a09fdc1c514e73572f
Daudi Mwanzia <daudimwanzia@gmail.com>
2024-04-13 20:30:53 +0300

    add get_token route

--- backend/app.py
+++ backend/app.py
@@ -303,9 +303,6 @@ def get_token():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
